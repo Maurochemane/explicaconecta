e67c556661ec1a06ed1add55b695d240cf91331c
Maurochemane <Mauroche,mane@gmail.com>
2025-05-07 02:54:52 +0200

    Alterei a ordem das p]aginas

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,8 @@
 
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import About from "@/components/About";
 import Services from "@/components/Services";
+import About from "@/components/About";
 import Promotion from "@/components/Promotion";
 import HowItWorks from "@/components/HowItWorks";
 import Testimonials from "@/components/Testimonials";
@@ -15,8 +15,8 @@ const Index = () => {
       <Header />
       <main>
         <Hero />
-        <About />
         <Services />
+        <About />
         <Promotion />
         <HowItWorks />
         <Testimonials />
