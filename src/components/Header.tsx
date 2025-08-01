eca9e92da0e8cb5574c2ba23c1ae1655fdfd0986
Maurochemane <Maurochemane@gmail.com>
2025-08-01 23:20:09 +0200

    feat:V1 texto do hero e botão call to action actualizados

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
+import {useState} from "react";
+import {Link} from "react-router-dom";
 
 import {
   Book,
@@ -17,69 +16,69 @@ import {
   EllipsisVertical,
   ArrowBigRight,
 } from "lucide-react";
-import { Button } from "@/components/ui/button";
-{/*const NavLink = ({titulo}) => (
+import {Button} from "@/components/ui/button";
+{
+  /*const NavLink = ({titulo}) => (
   <LinkScroll>
   {titulo}
 
   </LinkScroll>
-)*/}
+)*/
+}
+
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const names = {
+    ver: ["inicio", "sobre", "cursos", "Ferramentas", "Serviços", "Contacto"],
+  };
+
   return (
-    <header className="fixed top-2 rounded-md  left-0 right-0 md:left-64 md:right-64 bg-secondary/30 backdrop-blur-xl z-50  ">
+    <header className="fixed top-0 left-0 right-0 md:left-0 md:right-0 bg-primary z-50  ">
       <div className=" container mx-auto py-4 px-4 md:px-6">
         <div className="relative">
           {/* Logo */}
-         
 
-            
           {/* Desktop Menu */}
-          <nav className="hidden lg:flex transition-colors items-center space-x-8 justify-center ">
+          <nav className="hidden lg:flex transition-colors items-center space-x-16 justify-center ">
             <Link
               to="/#home"
-              className=" relative group font-medium text-blue-900 duration-500 hover:text-white "
+              className=" relative group font-medium text-white duration-500 hover:text-secondary "
             >
               <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
-              Início
+              {names.ver[0]}
             </Link>
             <Link
               to="/about"
-              className=" relative group font-semibold text-blue-900 duration-500 hover:text-white "
+              className=" relative group font-semibold text-white duration-500 hover:text-secondary"
             >
               <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
               cursos
             </Link>
             <Link
               to="/dicas-estudo"
-              className="relative group font-medium text-blue-900 duration-500 hover:text-white"
+              className="relative group font-medium  text-white duration-500 hover:text-secondary"
             >
               <span className="absolute left-0 -bottom-1 w-0 h-0.5 transition-all duration-300 bg-white group-hover:w-full"></span>
               Ferramentas
             </Link>
 
-            <Link
-              to="/"
-              className=""
-            >
+            <Link to="/" className="">
               <span className=" w-6 h-6 ">
-              <img src="Frame.png" 
-              alt="logo" 
-              className=" w-[30px] h-auto"/>
+                <img src="Frame.png" alt="logo" className=" w-[30px] h-auto" />
               </span>
             </Link>
 
             <Link
               to="/cadastro-explicadores"
-              className="relative group font-medium text-blue-900 duration-500 hover:text-white"
+              className="relative group font-medium text-white duration-500 hover:text-secondary"
             >
-             <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span> 
+              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
               Sobre
             </Link>
             <Link
               to="/cadastro-explicadores"
-              className="relative group font-medium text-blue-900 duration-500 hover:text-white"
+              className="relative group font-medium text-white duration-500 hover:text-secondary"
             >
               <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
               Serviços
@@ -87,49 +86,40 @@ const Header = () => {
 
             <Link
               to="/Contact"
-              className="relative group font-medium text-blue-900 duration-500 hover:text-white"
+              className="relative group font-medium text-white duration-500 hover:text-secondary"
             >
               <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
               Contacto
             </Link>
 
-
             <div className=" hidden md:flex cursor-pointer">
-            <div className=" flex items-center justify-center h-10 w-10 rounded-full bg-primary  border-2  ">
-              <p className=" text-white font-medium text-sm duration-500 hover:text-white ">EN</p>
+              <div className=" flex items-center justify-center h-10 w-10 rounded-full bg-primary  border-2  ">
+                <p className=" text-white font-medium text-sm duration-500 hover:text-white ">
+                  EN
+                </p>
+              </div>
             </div>
-            </div>
-         
           </nav>
-            
 
           {/* Register Button (Desktop) */}
-
-          
-
-         
-          
 
           {/* Mobile Menu Button */}
           <div className="lg:hidden flex items-center justify-between">
             <Link to="/">
-
-            <div className="h-8 w-8">
-            <img src="Frame.png" alt="logo" />
-            </div>
-
+              <div className="h-8 w-8">
+                <img src="Frame.png" alt="logo" />
+              </div>
             </Link>
-          <button
-            className="lg:hidden flex  text-white focus:outline-none"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            
-            {isMenuOpen ? (
-              <X className="h-6 w-6" />
-            ) : (
-              <Menu className="h-6 w-6" />
-            )}
-          </button>
+            <button
+              className="lg:hidden flex  text-white focus:outline-none"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              {isMenuOpen ? (
+                <X className="h-6 w-6" />
+              ) : (
+                <Menu className="h-6 w-6" />
+              )}
+            </button>
           </div>
         </div>
 
@@ -137,83 +127,60 @@ const Header = () => {
         {isMenuOpen && (
           <nav className="lg:hidden h-screen flex justify-center   text-center  ">
             <div className="flex flex-col  justify-center gap-8 text-3xl text-white">
-              
               <Link
                 to="/#home"
-                className="font-medium hover:text-primary transition-colors"
+                className="font-medium hover:text-secondary transition-colors "
                 onClick={() => {
                   setIsMenuOpen(false);
                   const el = document.getElementById("home");
-                  if (el) el.scrollIntoView({ behavior: "smooth" });
+                  if (el) el.scrollIntoView({behavior: "smooth"});
                 }}
               >
-             
-                <div className="flex items-center  font-semibold ">
-                  
-                  INÍCIO
-                </div>
+                <div className="flex items-center  font-semibold ">INÍCIO</div>
               </Link>
               <Link
                 to="/about"
-                className="font-medium hover:text-primary transition-colors"
+                className="font-medium hover:text-secondary transition-colors "
                 onClick={() => setIsMenuOpen(false)}
               >
-                <div className="flex items-center font-semibold  ">
-                 
-                 CURSOS
-                </div>
+                <div className="flex items-center font-semibold  ">CURSOS</div>
               </Link>
               <Link
                 to="/dicas-estudo"
-                className="font-medium hover:text-primary transition-colors"
+                className="font-medium hover:text-secondary transition-colors "
                 onClick={() => setIsMenuOpen(false)}
               >
                 <div className="flex items-center font-semibold ">
-                 
                   FERRAMENTAS
                 </div>
               </Link>
 
               <Link
                 to="/dicas-estudo"
-                className="font-medium hover:text-primary transition-colors"
+                className="font-mediumhover:text-secondary transition-colors "
                 onClick={() => setIsMenuOpen(false)}
               >
-                <div className="flex items-center font-semibold ">
-                 
-                  SERVIÇOS
-                </div>
+                <div className="flex items-center font-semibold ">SERVIÇOS</div>
               </Link>
 
               <Link
                 to="/dicas-estudo"
-                className="font-medium hover:text-primary transition-colors"
+                className="font-medium hover:text-secondary transition-colors "
                 onClick={() => setIsMenuOpen(false)}
               >
-                <div className="flex items-center font-semibold ">
-                
-                  SOBRE
-                </div>
+                <div className="flex items-center font-semibold ">SOBRE</div>
               </Link>
-
 
               <Link
                 to="/contact"
                 className=" relative group font-semibold  transition-colors"
                 onClick={() => setIsMenuOpen(false)}
               >
-             <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:min-w-full"></span>
-                  
-                <div className="flex items-center ">
-                  Contacto
-                </div>
-              </Link>
-              
-           
+                {/* <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:min-w-full"></span> */}
 
-              
+                <div className="flex items-center  hover:text-secondary transition-colors ">CONTACTO</div>
+              </Link>
             </div>
-            
           </nav>
         )}
       </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,23 +1,55 @@
-import AutoHeroCarousel from './AutoHeroCarousel';
-import { ArrowRight } from 'lucide-react';
-import { Link } from "react-router-dom";
+import AutoHeroCarousel from "./AutoHeroCarousel";
+import {ArrowRight} from "lucide-react";
+import {Link} from "react-router-dom";
 
 const Hero = () => {
-  
   return (
-    <section className=" relative h-screen min-h-[600px] bg-gradient-to-b from-secondary to-primary">
-      
-      {/* Conteúdo sobreposto */}
-     
-      
-      {/* Indicador de scroll (opcional) */}
-      <div className = "hidden md:block">
-      <div className=" absolute bottom-2 left-1/2 transform -translate-x-1/2 z-10 animate-bounce ">
-        <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
-          <div className="w-1 h-2 bg-white mt-2 rounded-full"></div>
+    <section className=" relative h-screen min-h-[600px] bg-gradient-to-b from-[#06FDE9] to-[#0079E7] ">
+      <div className="flex justify-center items-center h-[100%]  flex-col gap-3 px-4 text-center">
+        <h1 className=" text-5xl ">
+          Aprende mais
+          <br />
+          <span className="text-white">conectado </span>a quem sabe
+        </h1>
+        <p className="text-white ">
+          Conectamos voce aos melhores cenros de ensino presencial do pais,
+          agora
+          <br />
+          tambem com cursos online seguros e inovadores. Tudo na explicaconecta.{" "}
+        </p>
+          {/* Button ...Mc.... */}
+        <Link to="/cadastro-explicadores">
+        <div className=" 
+        mt-10 flex shadow-lg shadow-sec animate-shadow-glow justify-center items-center border-2 border-solid border-white rounded-[30px] w-[207px] h-[62px]  hover:outline hover:outline-2 hover:outline-white hover:outline-offset-4 transition-all duration-700">
+          
+          <button className="text-blue-600 bg-white rounded-[30px] w-[199px] h-[54px] z-10">
+            inscreva-se
+          </button>
+        </div>
+        </Link>
+      </div>
+
+
+      {/* Indicador de scroll*/}
+      <div className="hidden md:block">
+        <div className=" absolute bottom-2 left-1/2 transform -translate-x-1/2 z-10 animate-bounce ">
+          <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
+            <div className="w-1 h-2 bg-white mt-2 rounded-full"></div>
+          </div>
         </div>
       </div>
-      </div>
+
+        {/*imagem vectorizada*/}
+      <img src="./hero/vector.png" alt="vetor bg" 
+      className="absolute bottom-0 left-1/2 transform -translate-x-1/4 mix-blend-overlay opacity-30 "
+      />
+      
+      <img src="./hero/vector.png" alt="vetor bg" 
+      className="absolute bottom-0 left-1/4 transform -translate-x-1/2 mix-blend-overlay opacity-30 "
+      />
+
+      
+
     </section>
   );
 };
