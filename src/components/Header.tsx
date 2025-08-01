ee551e546724de6ffd99f96ad15e777022865b94
Maurochemane <Maurochemane@gmail.com>
2025-08-01 12:41:40 +0200

    feat: versao header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 import {
   Book,
   ChevronDown,
@@ -17,38 +18,44 @@ import {
   ArrowBigRight,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+{/*const NavLink = ({titulo}) => (
+  <LinkScroll>
+  {titulo}
 
+  </LinkScroll>
+)*/}
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="fixed top-0 left-64 right bg-secondary z-50">
+    <header className="fixed top-2 rounded-md  left-0 right-0 md:left-64 md:right-64 bg-secondary/30 backdrop-blur-xl z-50  ">
       <div className=" container mx-auto py-4 px-4 md:px-6">
         <div className="relative">
           {/* Logo */}
          
 
+            
           {/* Desktop Menu */}
-          <nav className="hidden lg:flex  items-center space-x-8 justify-center">
+          <nav className="hidden lg:flex transition-colors items-center space-x-8 justify-center ">
             <Link
               to="/#home"
-              className=" relative group font-medium text-blue-900"
+              className=" relative group font-medium text-blue-900 duration-500 hover:text-white "
             >
-              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-secondary transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
               Início
             </Link>
             <Link
               to="/about"
-              className=" relative group font-medium text-blue-900"
+              className=" relative group font-semibold text-blue-900 duration-500 hover:text-white "
             >
-              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-secondary transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
               cursos
             </Link>
             <Link
               to="/dicas-estudo"
-              className="relative group font-medium text-blue-900"
+              className="relative group font-medium text-blue-900 duration-500 hover:text-white"
             >
-              <span className="absolute left-0 -bottom-1 w-0 h-0.5 transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 w-0 h-0.5 transition-all duration-300 bg-white group-hover:w-full"></span>
               Ferramentas
             </Link>
 
@@ -65,31 +72,31 @@ const Header = () => {
 
             <Link
               to="/cadastro-explicadores"
-              className="relative group font-medium text-blue-900"
+              className="relative group font-medium text-blue-900 duration-500 hover:text-white"
             >
-              
+             <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span> 
               Sobre
             </Link>
             <Link
               to="/cadastro-explicadores"
-              className="relative group font-medium text-blue-900"
+              className="relative group font-medium text-blue-900 duration-500 hover:text-white"
             >
-              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-secondary transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
               Serviços
             </Link>
 
             <Link
               to="/Contact"
-              className="relative group font-medium text-blue-900"
+              className="relative group font-medium text-blue-900 duration-500 hover:text-white"
             >
-              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-secondary transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
               Contacto
             </Link>
 
 
-            <div className=" hidden md:flex">
+            <div className=" hidden md:flex cursor-pointer">
             <div className=" flex items-center justify-center h-10 w-10 rounded-full bg-primary  border-2  ">
-              <p className=" text-white font-medium text-sm  ">EN</p>
+              <p className=" text-white font-medium text-sm duration-500 hover:text-white ">EN</p>
             </div>
             </div>
          
@@ -192,11 +199,12 @@ const Header = () => {
 
               <Link
                 to="/contact"
-                className="font-semibold hover:text-primary transition-colors"
+                className=" relative group font-semibold  transition-colors"
                 onClick={() => setIsMenuOpen(false)}
               >
-                <div className="flex items-center ">
+             <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:min-w-full"></span>
                   
+                <div className="flex items-center ">
                   Contacto
                 </div>
               </Link>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,39 +8,7 @@ const Hero = () => {
     <section className=" relative h-screen min-h-[600px] bg-gradient-to-b from-secondary to-primary">
       
       {/* Conteúdo sobreposto */}
-      <div className="absolute inset-0 flex items-center md:max-w-max z-10"> 
-        <div className="container mx-auto px-0 flex md:justify-center ">
-          <div className="max-w-max lg:max-w-2xl bg-primary bg-opacity-25  backdrop-blur-sm p-8  rounded-xl text-white md:bg-black md:max-w-max">
-            <span className=" mt-10 md:mt-0 inline-block px-3 py-1 bg-primary text-sm font-semibold mb-0 rounded-full">
-              julho
-            </span>
-            
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight  text-white mb-4">
-              Aprende mais <span className="text-secondary">conectado </span>a quem sabe
-            </h1>
-            
-            <p className="text-lg md:text-xl mb-8 opacity-90">
-              Busca dominar as matérias? Conecte-se ao seu potencial máximo
-                com os nossos explicadores qualificados em Maputo e Matola.
-            </p>
-            
-            <div className="flex flex-col sm:flex-row gap-4">
-              <Link to="/solicitar-explicador"> 
-              <button className="bg-primary   text-white font-semibold py-3 px-6 rounded-lg transition flex items-center justify-center gap-2 hover:bg-white hover:text-primary hover:border-primary">
-              Começar Agora
-              <ArrowRight className="w-5 h-5" />
-              </button>
-              </Link>
-              
-              <Link to= "/about">
-              <button className="bg-transparent border-2 border-white hover:bg-white hover:text-black text-white font-semibold py-3 px-6 rounded-lg transition">
-                Saiba Mais
-              </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+     
       
       {/* Indicador de scroll (opcional) */}
       <div className = "hidden md:block">
